4cd2e9f3fb635e2d65edd09b5b8611ed708b634c
f0cuS <f0cuSmcsza@gmail.com>
2020-07-13 18:35:58 +0300

    Wrote view with temp local data.

--- Resume/Resume/App_Start/BundleConfig.cs
+++ Resume/Resume/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Resume
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/resume.css"));
         }
     }
 }
---
+++ Resume/Resume/Controllers/ResumeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume.Controllers
+{
+    public class ResumeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Certificates()
+        {
+            return View();
+        }
+
+    }
+}
